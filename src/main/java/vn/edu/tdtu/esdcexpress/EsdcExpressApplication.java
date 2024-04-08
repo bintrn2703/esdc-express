33588293cf13725b07a177cb02d5006bca20aae8
Tran Duc Phat <phatpro2003@gmail.com>
2024-04-08 22:17:49 +0700

    Initial Commit

---
+++ src/main/java/vn/edu/tdtu/esdcexpress/EsdcExpressApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.tdtu.esdcexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdcExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsdcExpressApplication.class, args);
+    }
+
+}
