28397a318ee195f320ed67f09baade4e28b00a6a
Tran Duc Phat <phatpro2003@gmail.com>
2024-04-09 13:48:57 +0700

    Fix not show css

--- src/main/java/vn/edu/tdtu/esdcexpress/EsdcExpressApplication.java
+++ src/main/java/vn/edu/tdtu/esdcexpress/EsdcExpressApplication.java
@@ -2,12 +2,33 @@ package vn.edu.tdtu.esdcexpress;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class EsdcExpressApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EsdcExpressApplication.class, args);
+    }
+
+    // Thymeleaf configuration to show static resources
+    @Bean
+    public ViewResolver viewResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode("XHTML");
+        templateResolver.setPrefix("views/");
+        templateResolver.setSuffix(".html");
+
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(engine);
+        return viewResolver;
     }
 
 }
