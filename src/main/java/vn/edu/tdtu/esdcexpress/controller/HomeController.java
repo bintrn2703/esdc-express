2f18648bf5c7ea45c2498637104ec3d3878ea435
anhkhoaIT <super.khoa14@gmail.com>
2024-04-13 10:55:38 +0700

    Add func export csv

--- src/main/java/vn/edu/tdtu/esdcexpress/controller/HomeController.java
+++ src/main/java/vn/edu/tdtu/esdcexpress/controller/HomeController.java
@@ -1,6 +1,8 @@
 package vn.edu.tdtu.esdcexpress.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,9 +16,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import vn.edu.tdtu.esdcexpress.model.Order;
 import vn.edu.tdtu.esdcexpress.model.User;
 import vn.edu.tdtu.esdcexpress.model.UserRegistrationDto;
+import vn.edu.tdtu.esdcexpress.service.OrderService;
 import vn.edu.tdtu.esdcexpress.service.UserService;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -24,6 +32,9 @@ public class HomeController {
     UserService userService;
     @Autowired
     AuthenticationManager authenticationManager;
+
+    @Autowired
+    OrderService orderService;
 
     @GetMapping("/")
     public String index() {
@@ -87,5 +98,30 @@ public class HomeController {
         newUser.setEmail(userDto.getEmail());
         userService.save(newUser);
         return "redirect:/custom-login";
+    }
+
+    // Trong controller của bạn
+    @GetMapping("/order/export-csv")
+    public void exportToCSV(HttpServletResponse response, HttpSession session) throws IOException {
+        String accountName = (String) session.getAttribute("accountName");
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; file=orders.csv");
+
+        User user = userService.getUserByName(accountName);
+        List<Order> orders = (List<Order>) orderService.getOrdersByUsername(user);
+
+        // Tạo đối tượng PrintWriter từ response
+        PrintWriter writer = response.getWriter();
+
+        // Viết tiêu đề cho các cột trong file CSV
+        writer.write("Order ID, Status, Pickup Name, Pickup Phone, Pickup Address, Deliver Name, Deliver Phone, Deliver Address, Parcel Name, Item Quantity, Delivery Instruction, Shipping Fee Payment, Allow Mutual Check, COD, Parcel Value, Collect Type, Shipping Service, Shipping Fee, Create At, Pickup At, Delivered At\n");
+
+        // Duyệt qua danh sách đơn hàng và viết thông tin của mỗi đơn hàng vào file CSV
+        for (Order order : orders) {
+            writer.write(order.getId() + "," + order.getStatus() + "," + order.getPickup_name() + "," + order.getPickup_phone() + "," + order.getPickup_address() + "," + order.getDeliver_name() + "," + order.getDeliver_phone() + "," + order.getDeliver_address() + "," + order.getParcel_name() + "," + order.getItem_quantity() + "," + order.getDelivery_instruction() + "," + order.getShipping_fee_payment() + "," + order.getAllow_mutual_check() + "," + order.getCod() + "," + order.getParcel_value() + "," + order.getCollect_type() + "," + order.getShipping_service() + "," + order.getShipping_fee() + "," + order.getCreate_at() + "," + order.getPickup_at() + "," + order.getDelivered_at() + "\n");
+        }
+
+        writer.flush();
+        writer.close();
     }
 }
